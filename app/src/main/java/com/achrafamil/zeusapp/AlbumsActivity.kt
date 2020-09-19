ffe965eb08d3905713a6be6ec679bac241f99b00
Achraf Amil <achraf.amil@gmail.com>
2020-09-19 21:30:55 +0200

    Initial commit

---
+++ app/src/main/java/com/achrafamil/zeusapp/AlbumsActivity.kt
@@ -0,0 +1,11 @@
+package com.achrafamil.zeusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
