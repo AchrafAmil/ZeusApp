c9e6a688114d22cc0c8191437845c0fd852fd4c4
Achraf Amil <achraf.amil@gmail.com>
2020-09-21 21:39:26 +0200

    Handle error

--- app/src/main/java/com/achrafamil/zeusapp/albums/AlbumsViewModel.kt
+++ app/src/main/java/com/achrafamil/zeusapp/albums/AlbumsViewModel.kt
@@ -1,17 +1,22 @@
 package com.achrafamil.zeusapp.albums
 
+import android.content.Context
+import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.achrafamil.zeusapp.common.core.TrackRepository
+import com.achrafamil.zeusapp.utils.toast
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class AlbumsViewModel @ViewModelInject constructor(
     private val trackRepository: TrackRepository,
-    private val uiModelMapper: UiModelMapper
+    private val uiModelMapper: UiModelMapper,
+    @ApplicationContext private val context: Context
 ) : ViewModel() {
 
     val uiModel: LiveData<AlbumsUiModel> = trackRepository
@@ -21,7 +26,17 @@ class AlbumsViewModel @ViewModelInject constructor(
 
     init {
         viewModelScope.launch {
-            trackRepository.syncTracks()
+            try {
+                trackRepository.syncTracks()
+            } catch (exception: Exception) {
+                val message = "Failed to sync with server, check internet."
+                Log.e(LOG_TAG, message, exception)
+                context.toast(message)
+            }
         }
+    }
+
+    companion object {
+        private const val LOG_TAG = "AlbumsViewModel"
     }
 }
